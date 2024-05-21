05d9cc4267bb262a972038d0171da532eeff6cd4
jeccles1 <jeccles1@emich.edu>
2024-05-21 10:18:33 -0400

    step7 of the lab

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -3,6 +3,7 @@ public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		System.out.println("Line #2");
+		System.out.println("Programmer 2 did this");
 	}
 
 }
